2e31287e93454981d5bd8272e20e9f09e18dc096
Paul Woods <mr.paul.woods@gmail.com>
2012-02-06 10:01:34 -0600

    Updated version and plugin info.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -24,3 +24,14 @@ grails.project.dependency.resolution = {
 		build (":release:1.0.1") { export = false }
 	}
 }
+
+boolean snapshot = new File("application.properties").text.contains("-SNAPSHOT")
+
+if(snapshot) {
+	grails.project.repos.default = "snapshot"
+	grails.project.repos.snapshot.url = "http://dta0459765.am.dhcp.ti.com:9003/artifactory/libs-snapshot-local"
+} else {
+	grails.project.repos.default = "release"
+	grails.project.repos.release.url = "http://dta0459765.am.dhcp.ti.com:9003/artifactory/libs-release-local"
+}
+
